da0c9a62acdf70b4f0f2efed36e666998428e02d
aurtabatamindo <aurta.batamindo@gmail.com>
2022-11-09 16:06:17 +0700

    news carouselview

--- app/src/main/java/com/example/isss/MainActivity.java
+++ app/src/main/java/com/example/isss/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.isss;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.helper.widget.Carousel;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class MainActivity extends AppCompatActivity {
+    CarouselView news;
+            int[] sampleImages = {R.drawable.sad_icon, R.drawable.sad_icon, R.drawable.sad_icon, R.drawable.sad_icon, R.drawable.sad_icon};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        news = findViewById(R.id.news_carousel);
+        news.setPageCount(sampleImages.length);
+        news.setImageListener(imageListener);
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 }
