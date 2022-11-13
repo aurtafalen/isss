0c222a014ada066ce5050eb7c4098a0fe7dcb4eb
unknown <aurta.batamindo@gmail.com>
2022-11-14 06:42:26 +0700

    sdk zello

--- app/src/main/java/com/example/isss/HandlyTalky.java
+++ app/src/main/java/com/example/isss/HandlyTalky.java
@@ -11,14 +11,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.zello.sdk.Zello;
+
 public class HandlyTalky extends AppCompatActivity {
     CardView mic;
     ImageView icon_mic;
     TextView txt_mic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handly_talky);
+
+        Zello.getInstance().configure("com.loudtalks", this);
 
         icon_mic = findViewById(R.id.icon_mic);
         mic = findViewById(R.id.btn_mic);
@@ -29,11 +34,15 @@ public class HandlyTalky extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
+                        Zello.getInstance().beginMessage();
+
                         icon_mic.setColorFilter(Color.RED);
                         txt_mic.setTextColor(Color.RED);
                         break;
 
                     case MotionEvent.ACTION_UP:
+                        Zello.getInstance().endMessage();
+
                         icon_mic.setColorFilter(Color.WHITE);
                         txt_mic.setTextColor(Color.WHITE);
                         break;
@@ -43,4 +52,5 @@ public class HandlyTalky extends AppCompatActivity {
         });
 
     }
+
 }
